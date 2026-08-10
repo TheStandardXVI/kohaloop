d3c9098f522448d8a4464bbd2f2d5cee29bc36b0
TheStandardXVI <quelqunquelqun1@gmail.com>
2026-08-10 21:41:21 +1200

    Version Initiale

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'kohaloop-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first : va toujours chercher la derniere version en ligne d'abord,
+// et n'utilise le cache que si le reseau est indisponible (mode hors-ligne).
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
